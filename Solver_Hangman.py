59e5584d08ad5d8903dc51b606b70ea349ece693
Feng Siyao <fengsy868@gmail.com>
2016-10-27 18:24:27 +0200

    Finished

--- Solver_Hangman.py
+++ Solver_Hangman.py
@@ -5,24 +5,134 @@
 
 import httplib
 import json
+from binarytree import tree, convert, heap, pprint
+import pickle
 
+#----------------------------------------------------------------------------------------------------------
+# Build connection with server
 conn = httplib.HTTPSConnection("hangman.leanapp.cn")
 
 payload = "-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"username\"\r\n\r\nsiyaofeng\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"password\"\r\n\r\ndaa9d0effc7021d4c9636094f211c748\r\n-----011000010111000001101001--"
 
-headers = {
+headers1 = {
     'content-type': "multipart/form-data; boundary=---011000010111000001101001",
     'authorization': "Basic c2l5YW9mZW5nOmRhYTlkMGVmZmM3MDIxZDRjOTYzNjA5NGYyMTFjNzQ4",
     'cache-control': "no-cache",
     'postman-token': "8acf63b3-68df-ff4a-3088-6c39a23c440a"
     }
 
-conn.request("POST", "/login", payload, headers)
+conn.request("POST", "/login", payload, headers1)
 
 res = conn.getresponse()
 data = res.read()
 
 r1 = json.loads(data)
 myToken = r1["token"]
-print myToken
-# print(data.decode("utf-8"))
+print 'This is my token', myToken
+
+headers2 = {
+    'content-type': "multipart/form-data; boundary=---011000010111000001101001",
+    'authorization': "Basic c2l5YW9mZW5nOmRhYTlkMGVmZmM3MDIxZDRjOTYzNjA5NGYyMTFjNzQ4",
+    'cache-control': "no-cache",
+    'postman-token': "8acf63b3-68df-ff4a-3088-6c39a23c440a",
+    'X-Token': myToken
+    }
+
+conn.request("GET", "/me", payload, headers2)
+
+r2 = json.loads(conn.getresponse().read())
+# print r2
+
+#----------------------------------------------------------------------------------------------------------
+#import the trees built from file
+print "Loading the tree, this may take a few time"
+resultfile = open('result.txt', 'r')
+thelist = pickle.load(resultfile)
+treelist = []
+
+for i in thelist:
+	treelist.append(convert(i))
+#----------------------------------------------------------------------------------------------------------
+# functions for playing
+
+def guess_letter_ingame(gameid, roundid, guess):
+	guessurl = "/game/"+str(gameid)+"/round/"+str(roundid)+"?guess="+guess
+	conn.request("POST", guessurl, payload, headers2)
+
+	return json.loads(conn.getresponse().read())
+
+
+
+#----------------------------------------------------------------------------------------------------------
+# start playing
+
+#start a new game
+conn.request("POST", "/game", payload, headers2)
+res0 = json.loads(conn.getresponse().read())
+
+# "get game info"
+conn.request("GET", "/game", payload, headers2)
+res1 = json.loads(conn.getresponse().read())
+gameid = res1['games'][0]['id']  #gameid
+print "Game ID is ",gameid
+
+
+
+print "Guess start!"
+for i in xrange(20):
+	# "start a new round in gameid"
+	conn.request("POST", "/game/"+str(gameid), payload, headers2)
+	res2 = json.loads(conn.getresponse().read())
+	roundid = res2["current_round_number"]
+	print "Current round ID is ",roundid
+
+
+	# "get the round info in gameid"
+	conn.request("GET", "/game/"+str(gameid)+"/round/"+str(roundid), payload, headers2)
+	res3 = json.loads(conn.getresponse().read())
+
+
+
+	word_length = len(res3['word'])
+	previous_count = word_length
+
+	node_tree = treelist[word_length-1]
+	while res3["status"] == "in progress":
+		count = 0
+		letter_guess = node_tree.value
+
+		res3 = guess_letter_ingame(gameid,roundid,letter_guess)
+		res3 = res3["round"]
+		print res3["word"]
+
+		for i in res3["word"]:  #count the _ in the word to determine right or wrong guess
+			if i == '_':
+				count += 1
+
+		if node_tree.left == None: #in case the tree reach max height
+			print "Unfortunatly, max height reached"
+			for i in range(res3['life']): #guess all a to pass this round
+				temp = guess_letter_ingame(gameid,roundid,'a')
+
+		if count == previous_count:  	#wrong guess
+			node_tree = node_tree.left
+		else:							#right guess
+			node_tree = node_tree.right
+
+		previous_count = count
+	
+
+# Show the final score
+conn.request("GET", "/game/"+str(gameid), payload, headers2)
+res4 = json.loads(conn.getresponse().read())
+print "Guess finished, your final score is ", res4["score"]
+
+
+
+
+
+
+
+
+
+
--- tree_generator.py
+++ tree_generator.py
@@ -1,8 +1,8 @@
 #this programme generate a set of decision trees for Hangman according to all the English words
 # I have used Python tree class implemented by Joowani https://github.com/joowani/binarytree
 
-
-from binarytree import tree, bst, convert, Node, pprint
+import pickle
+from binarytree import tree, bst, convert, heap, Node, pprint
 
 print 'Start reading the dictionary file...'
 with open('words.txt') as f:
@@ -15,26 +15,12 @@ for word in allWords:
 	if len(word) > maxlen:
 		maxlen = len(word)
 
-print 'The longest word has '+str(maxlen)+' letters in it'
-
-# # Generate a random binary tree and return its root
-# my_tree = tree(height=5, balanced=False)
-
-# # Generate a random BST and return its root
-# my_bst = bst(height=5)
-
-
-# # Pretty print the trees in stdout
-# pprint(my_tree)
-# pprint(my_bst)
-
-# my_list = convert(my_bst)
-# print my_list
+print 'The longest word has '+str(maxlen)+' letters'
 
 treelist=[];wordlengthlist=[];
-
+height_tree = 18
 for n in xrange(maxlen):						 #Create a list of binary search trees and a list of lists of different length words
-	treelist.append(bst(height=10))
+	treelist.append(heap(height=height_tree, max=True))
 	wordlengthlist.append([])
 
 for word in allWords:
@@ -42,25 +28,97 @@ for word in allWords:
 
 print 'There are '+str(len(wordlengthlist[8]))+' Word with 9 letters'
 
-def find_most_frequent_letter(length):
+#This function aims at find the (rank)th frequent letter in a word's list
+def find_most_frequent_letter(wordlist,rank):
 	count=[0]
 	for n in xrange(26):
 		count.append(0)
 
-	for word in wordlengthlist[length-1]:  #find most frequent letter in 6 length word
+	for word in wordlist:  #find most frequent letter in 6 length word
 		for letter in list(word):
 			if letter.isalpha():    #to avoid the char <<'>>
 				count[ord(letter) - 97] += 1
 
-	print count, type(count)
-	print 'the most frequent letter of length ',length,'is', chr(count.index(max(count))+97)
+	new = count[:]	#copy the rank list 
+	for i in xrange(rank-1):
+		new.remove(max(new))	#delete the most biggest number in new list
+
+	return chr(count.index(max(new))+97) #return the (rank)th frequent letter in wordlist
+
+
+# These twofunction used to delete word do/dont contains certain letter from the wordlist
+def generate_new_list_with_letter_correct(letter, wordlist):
+	temp_list= []
+	for word in wordlist:
+		if letter in word:
+			temp_list.append(word.replace(letter,''))
+	wordlist[:] = []
+
+	for word in temp_list:
+		wordlist.append(word)
+
+
+
+
+def generate_new_list_with_letter_wrong(letter, wordlist):
+	temp_list = []
+	for word in wordlist:
+		if letter in word:
+			temp_list.append(word)
+
+	for word in temp_list:
+		wordlist.remove(word)
+	
+# main iteration function to fill the tree
+def fill_the_tree(node,wordlist,deepth,height):
+	
+	if deepth == height+1:
+		return deepth-1
+	
+	local_wordlist1 = wordlist[:] #this for wrong guess
+	local_wordlist2 = wordlist[:] #this for correct guess
+
+
+
+
+
+	#get the value for left node, wrong guess
+	generate_new_list_with_letter_wrong(node.value, local_wordlist1)
+
+	node.left.value = find_most_frequent_letter(local_wordlist1, 1)   #left node contains the next most frequent letter if guess is wrong
+	deepth = fill_the_tree(node.left, local_wordlist1, deepth+1, height)
+	
+
+	#get the value for right node, correct guess
+	generate_new_list_with_letter_correct(node.value, local_wordlist2)
+	node.right.value = find_most_frequent_letter(local_wordlist2, 1)
+	deepth = fill_the_tree(node.right, local_wordlist2, deepth+1, height)
+	
+	return deepth-1
 
 #list of 31 most common letter for different length
-first_guess = ['A','A','A','A','S','E','E','E','E','E','E','E','I','I','I','I','I','I','I','I','I','I','I','I','I','I','I','I','I','I','I']
+first_guess = ['a','a','a','a','s','e','e','e','e','e','e','e','i','i','i','i','i','i','i','i','i','i','i','i','i','i','i','i','i','i','i']
 
-#Fill the 31 binary trees for different length
+find_most_frequent_letter(wordlengthlist[12],1)
+
+# Fill the 31 binary trees for different length
 for i in xrange(31): 
+	print 'Building the tree for word length ',i
 	treelist[i].value = first_guess[i]
-	
+	wordlist = wordlengthlist[i]
+	fill_the_tree(treelist[i], wordlist, 1, height_tree)
+
+
+final_list = []
+for tree in treelist:
+	final_list.append(convert(tree))
+
+resultfile = open('result.txt', 'w')
+pickle.dump(final_list, resultfile)
+
+
+
+
+
 
 
